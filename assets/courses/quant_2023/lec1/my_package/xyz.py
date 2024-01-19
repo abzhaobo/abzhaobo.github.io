dd75bee5ecb7312db5e889b754b2fb633bdd93f3
abzhaobo <zbozhaob@gmail.com>
2024-01-19 02:57:28 +0000

    Deploying to gh-pages from @ abzhaobo/abzhaobo.github.io@7be802588ae69dc4ea6727ae4ac2191258fa4823 🚀

---
+++ assets/courses/quant_2023/lec1/my_package/xyz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"An example of module"
+
+def my_print():
+    print(' Roses are red,\n',
+          'Violets are blue,\n',
+          'Whatever you say,\n',
+          'is always true. asdasd')
+
+
+if __name__ == '__main__':
+    my_print()
