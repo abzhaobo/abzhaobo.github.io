44cea0bbef9109bc194802aeda28e1a1c49a4fdd
Bo Zhao <bozhao@Bos-MBP-2024.local>
2025-02-28 10:45:27 +0800

    Build website

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,30 +6,25 @@ $(document).ready(function () {
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".mytake.hidden.open").toggleClass("open");
   });
-
   $("a.award").click(function () {
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".mytake.hidden.open").toggleClass("open");
   });
-
   $("a.bibtex").click(function () {
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
     $(this).parent().parent().find(".mytake.hidden.open").toggleClass("open");
   });
-
   $("a.mytake").click(function () {
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".mytake.hidden").toggleClass("open");
-  });
-
+  }); 
   $("a").removeClass("waves-effect waves-light");
-});
 
   // bootstrap-toc
   if ($("#toc-sidebar").length) {
